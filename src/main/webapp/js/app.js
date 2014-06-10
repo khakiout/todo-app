78b34cd1e516fa215dd4005c7619b5accf71f7c9
ruel <rzapanta@stratpoint.com>
2014-06-10 15:57:44 +0800

    Add new task

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -10,30 +10,42 @@ angular.module('todoApp', [])
 			
 			$scope.tasks = [ 
         {
-        	'name' : 'Wash clothes',
-        	'todoStamp' : new Date().getTime(),
-        	'state' : $scope.STATE_TODO
+        	name : 'Wash clothes',
+        	todoStamp : new Date().getTime(),
+        	state : $scope.STATE_TODO
         },
         {
-        	'name' : 'Finish report',
-        	'todoStamp' : new Date('01/01/2013').getTime(),
-        	'state' : $scope.STATE_TODO
+        	name : 'Finish report',
+        	todoStamp : new Date('01/01/2013').getTime(),
+        	state : $scope.STATE_TODO
         },
         {
-        	'name' : 'Buy Grocery',
-        	'todoStamp' : new Date('06/06/2014').getTime(),
-        	'state' : $scope.STATE_WIP
+        	name : 'Buy Grocery',
+        	todoStamp : new Date('06/06/2014').getTime(),
+        	state : $scope.STATE_WIP
         },
         {
-        	'name' : 'Cook Breakfast',
-        	'todoStamp' : new Date('06/06/2014').getTime(),
-        	'state' : $scope.STATE_DONE
+        	name : 'Cook Breakfast',
+        	todoStamp : new Date('06/06/2014').getTime(),
+        	state : $scope.STATE_DONE
         },
         {
-        	'name' : 'Prepare Room',
-        	'todoStamp' : new Date('06/06/2014').getTime(),
-        	'state' : $scope.STATE_DONE
+        	name : 'Prepare Room',
+        	todoStamp : new Date('06/06/2014').getTime(),
+        	state : $scope.STATE_DONE
         }
       ];
+			
+			$scope.addTask = function() {
+				console.log($scope.newTask);
+				var task = {
+				  name : $scope.newTask,
+				  todoStamp : new Date().getTime(),
+				  state : $scope.STATE_TODO
+				}
+				
+				$scope.tasks.push(task);
+				delete $scope.newTask;
+			}
 		}]
 	);
